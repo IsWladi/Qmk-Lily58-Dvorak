65e42cc6126b63bd32c7d1b1d672a18cd931423c
Isaac <wucisaac@outlook.com>
2023-01-29 20:22:12 -0300

    layers VIM y VIMI funcionan con errores y el color RGB se desincroniza, el derecho queda en rojo

--- keymap.c
+++ keymap.c
@@ -10,7 +10,8 @@ enum layer_number {
     _HIGHER,
     _ADJUST,
     _NUMPAD,
-    _VIM,
+    _VIM, //5
+    _VIMI, //6
 };
 
 // Tap Dance definitions
@@ -40,6 +41,7 @@ enum {
 enum custom_keycodes {
   RECORTE = SAFE_RANGE,
   BUSCADOR,
+  INSERT,
   MAYUS,
   MAX,
   MIN,
@@ -128,6 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTRL,          TD(TD_DOTS),    KC_Q,           KC_J,           KC_K,    KC_X, TD(TD_LEFT_K),   TD(TD_RIGHT_K), KC_B,    KC_M,    KC_W,    KC_V,         KC_Z,    KC_RCTRL, \
                                                    KC_LALT, KC_LGUI, LT(_LOWER,KC_SPC), KC_ESC,                       RECORTE, LT(_HIGHER,KC_ENT), KC_ESC, KC_RGUI \
                                                    ),
+
     /* LOWER
      * ,---------------------------------------------------.                    ,------------------------------------------------------.
      * |DEL             |  F1  |  F2  |  F3  |  F4  |  F5  |                    |      |      |  <<  | PLAY |  >>  |  BackSP           |
@@ -210,8 +213,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    XXXXXXX, \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX \
-    )
+                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, INSERT,  XXXXXXX, XXXXXXX, XXXXXXX \
+    ),
+
+    [_VIMI] = LAYOUT( \
+    KC_DEL,            KC_1,     KC_2,           KC_3,           KC_4,    KC_5,                                  KC_6,    KC_7,    KC_8,    KC_9,         KC_0,    TD(TD_PSCREN), \
+    MAYUS,           TD(TD_TILDE),   TD(TD_MINT),    TD(TD_BIGT),    KC_P,    KC_Y,                                  KC_F,    KC_G,    KC_C,    KC_H,         KC_L,    RSFT_T(KC_CAPS), \
+    LSFT_T(KC_TAB),    KC_A,           KC_O,           KC_E, LT(_NUMPAD,KC_U),     KC_I,                                  KC_D,    KC_R,    KC_T,    TD(TD_NN),    KC_S,    KC_BSPC, \
+    KC_LCTRL,          TD(TD_DOTS),    KC_Q,           KC_J,           KC_K,    KC_X, TD(TD_LEFT_K),   TD(TD_RIGHT_K), KC_B,    KC_M,    KC_W,    KC_V,         KC_Z,    KC_RCTRL, \
+                                                   KC_LALT, KC_LGUI, LT(_LOWER,KC_SPC), KC_ESC,                       INSERT, LT(_HIGHER,KC_ENT), KC_ESC, KC_RGUI \
+                                                   ),
     };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -362,6 +373,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }else {
         // Cuando la tecla es liberada
         clear_keyboard();
+      }
+      break;
+
+      case INSERT:
+      if (record->event.pressed) {
+        if (IS_LAYER_ON(_VIMI)) {
+          layer_off(_VIMI);
+          SEND_STRING(SS_TAP(X_ESC));
+        } else {
+          layer_on(_VIMI);
+          SEND_STRING(SS_TAP(X_I));
+        }
+        return true;
       }
       break;
     }
